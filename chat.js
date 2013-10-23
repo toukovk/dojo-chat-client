b8102b82039ecd47c0d74079b595a4b152b561ef
Touko Vainio-Kaila <touko.vainio-kaila@futurice.com>
2013-10-23 18:52:02 +0300

    Backend integraztion

--- chat.js
+++ chat.js
@@ -1,34 +1,79 @@
 angular.module('dojo-chat', [])
   .service('ChatService', [
     function() {
-      var messages = [
-        {text:'learn angular'},
-        {text:'build an angular app'}
-      ];
+      var data = {
+        messages: [
+          {sender: 'XYZ', timestamp: 1382541399510, message:'learn angular'},
+          {sender: 'XYZ', timestamp: 1382541399510, message:'build an angular app'}
+        ]
+      };
+      var backendCallback;
 
-      function getMessages() {
-        return messages;
+      function apply() {
+        if(backendCallback) {
+          backendCallback();
+        }
       }
+
+      var chat = new ChatConnector("http://192.168.102.66:8080");
+      
+      chat.handleAllRooms(function (rooms){
+        console.log("all the rooms");
+        console.log(rooms);
+      });
+
+      chat.handleJoinedRooms(function (rooms){
+        console.log("joined rooms");
+        console.log(rooms);
+      });
+
+      chat.handleUsersInRoom(function (roomUsers){
+        console.log("users in room");
+        console.log(roomUsers);
+      });
+      chat.handleReceiveMessage(function (message){
+        data.messages.push(message);
+        apply();
+        console.log("received message");
+        console.log(message);
+      });
+
+      var username = 'Touko';
+      chat.login(username);
+      chat.joinRoom('dojo');
+      window.chat = chat;
 
       function addMessage(text) {
-        messages.push({text:text});
+        var message = {
+          message: text,
+          timestamp: new Date().getTime(),
+          sender: username
+        }
+        data.messages.push(message);
+      }
+      function setBackendCallback(callback) {
+        backendCallback = callback;
       }
       return {
-        getMessages: getMessages,
-        addMessage: addMessage
+        data: data,
+        addMessage: addMessage,
+        setBackendCallback: setBackendCallback
       }
   }])
   .directive('message', function() {
     return {
       restrict: 'E',
       transclude: true,
-      template: '<li><span>{{msg.text}}</span></li>'
+      template: '<li><span>{{msg.message}}</span></li>'
     };
   })
   // <span>{{message.text}}</span>
   .controller('ChatCtrl', ['$scope', 'ChatService',
     function($scope, ChatService) {
-      $scope.messages = ChatService.getMessages();
+      ChatService.setBackendCallback(function() {
+        $scope.$apply();
+      });
+      $scope.data = ChatService.data;
      
       $scope.addMessage = function() {
         ChatService.addMessage($scope.todoText);
